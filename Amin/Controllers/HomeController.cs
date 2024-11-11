650c38571a3e8f96edde1b65deeb617c7c188bac
Nguyễn Cao Trí <caotri20012005@gmail.com>
2024-11-11 10:27:38 +0700

    update 10:27 11/11/2024

--- Amin/Controllers/HomeController.cs
+++ Amin/Controllers/HomeController.cs
@@ -1,16 +1,18 @@
 ﻿using Amin.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Amin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
         private readonly PatientManagementContext _context;
 
-        public HomeController(PatientManagementContext context)
+        public HomeController(ILogger<HomeController> logger, PatientManagementContext context)
         {
+            _logger = logger;
             _context = context;
         }
 
@@ -21,6 +23,34 @@ namespace Amin.Controllers
                 .Take(10) // Lấy 10 bài viết mới nhất
                 .ToListAsync();
             return View(posts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
